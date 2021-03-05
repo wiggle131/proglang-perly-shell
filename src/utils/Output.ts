105908ecce52e3a1da579e26f37f98ce10bc9b90
wiggle131 <jhosiecharlesespantaleon@gmail.com>
2021-03-05 10:58:41 +0800

    Update Output.ts

--- src/utils/Output.ts
+++ src/utils/Output.ts
@@ -57,35 +57,3 @@ export default function Output(
     });
    return output;
 }
- 
-export function CheckSyntax(
-  statement: ActualValue[],
-  variables: Variable[]
-  ): ExecuteOutput {
-
-      const output: ExecuteOutput = {
-      output: '',
-      status: false,
-      }
-
-  let actualOutput = '';
-
-  statement.forEach((token) => {
-      if (output.status) {
-        return;
-      }
-          console.log(token)
-          if(token.type === constantTypes.VAR){
-              const thisVar =  variables.find((variable: Variable)=> {
-                  return variable.identifier === token.value;
-              })
-              actualOutput += thisVar?.value?.toString() ?? '';
-              console.log(thisVar)
-          }
-  });
-
-  output.output = actualOutput;
-  return output;
-  }
-    
-    
